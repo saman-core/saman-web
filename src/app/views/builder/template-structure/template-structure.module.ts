b421b559b78feddc588a411ff6606ebc6606cb69
Hernael <hperer@movealong.tech>
2024-02-21 00:43:39 -0400

    init template builder

--- src/app/views/builder/template-structure/template-structure.module.ts
+++ src/app/views/builder/template-structure/template-structure.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatTreeModule } from '@angular/material/tree';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 import { ConfigurableDataEntityModule } from '@saman-core/common';
 import { TemplateStructureComponent } from './template-structure/template-structure.component';
 import { TemplateStructureRoutingModule } from './template-structure-routing.module';
+import { TemplateBuilderModule } from '@saman-core/data';
 
 @NgModule({
   imports: [
     TemplateStructureRoutingModule,
     CommonModule,
+    MatListModule,
     ReactiveFormsModule,
     ConfigurableDataEntityModule,
-    MatTreeModule,
-    MatButtonModule,
-    MatIconModule,
+    TemplateBuilderModule,
   ],
   declarations: [TemplateStructureComponent],
 })
