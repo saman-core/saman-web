19f0059a96f9a4e27134261e10960cac48925047
Hernael <hperer@movealong.tech>
2024-03-01 21:31:51 -0400

    update template-conditions

--- src/app/views/builder/template-conditions/template-conditions/template-conditions.component.ts
+++ src/app/views/builder/template-conditions/template-conditions/template-conditions.component.ts
@@ -5,18 +5,19 @@ import { DynamicDataSource } from './dynamic-data-source';
 import { TemplateConditionDialogComponent } from '../template-condition-dialog/template-condition-dialog.component';
 import { combineLatestWith } from 'rxjs/operators';
 import { MatTable } from '@angular/material/table';
+import { ConditionsPropertyModel } from '@saman-core/data/lib/module/template-builder/model/conditions-property.model';
+import { ConditionTypeEnum } from '@saman-core/data/lib/module/template-builder/model/condition-type.enum';
 
 @Component({
   selector: 'app-template-conditions',
   templateUrl: './template-conditions.component.html',
-  styleUrl: './template-conditions.component.scss'
+  styleUrl: './template-conditions.component.scss',
 })
 export class TemplateConditionsComponent {
+  static readonly NOT_FOUND = ' [Not_Found]';
   @ViewChild(MatTable) table: MatTable<unknown>;
   displayedColumns: string[] = ['property', 'value', 'visible', 'disable', 'alert', 'validate'];
   elementData: ConditionNodes[] = [];
-
-
   @ViewChild('dynamicEditorLoader', { read: ViewContainerRef, static: true })
   dynamicEditorLoader: ViewContainerRef;
   step = 0;
@@ -24,6 +25,8 @@ export class TemplateConditionsComponent {
   treeControl: FlatTreeControl<DynamicFlatNode>;
   dataSource: DynamicDataSource;
   templateNameSelected = '';
+  productNameSelected = '';
+  conType = ConditionTypeEnum;
 
   constructor(private _resourceRepository: ResourceRepository) {
     this.treeControl = new FlatTreeControl<DynamicFlatNode>(this.getLevel, this.isExpandable);
@@ -42,33 +45,17 @@ export class TemplateConditionsComponent {
 
   openDialog(productName: string, templateName: string) {
     this.elementData = [];
+    this.table.renderRows();
     const templateObservable = this._resourceRepository.getTemplate(productName, templateName);
-    const conditionsObservable = this._resourceRepository.getAllConditionsPropertiesByTemplate(productName, templateName);
-    const combinedObservable = templateObservable.pipe(
-      combineLatestWith(conditionsObservable)
+    const conditionsObservable = this._resourceRepository.getAllConditionsPropertiesByTemplate(
+      productName,
+      templateName,
     );
-
+    const combinedObservable = templateObservable.pipe(combineLatestWith(conditionsObservable));
     combinedObservable.subscribe(([node, conditions]) => {
-      const json = JSON.parse(atob(node.content));
-      const properties: string[] = json['properties'];
-      this.table.renderRows();
-
-      properties.forEach(p => this.elementData.push(new ConditionNodes(p)));
-
-
-      console.log(this.elementData);
-      
-      conditions.forEach(c => {
-        const found = this.elementData.find(e => e.property.toLowerCase() === c.property.toLowerCase());
-        if (typeof found === 'undefined') {
-          console.error(c);
-        } else {
-          console.log(c);
-        }
-      });
-      this.table.renderRows();
-
+      this._fillElementData(node, conditions);
       this.templateNameSelected = templateName;
+      this.productNameSelected = productName;
       this.step = 1;
     });
   }
@@ -76,14 +63,62 @@ export class TemplateConditionsComponent {
   setStep(index: number) {
     this.step = index;
   }
+
+  public isUndefined(v): boolean {
+    return typeof v === 'undefined';
+  }
+
+  public createDmn(productName: string, templateName: string, conditionType: ConditionTypeEnum) {
+    console.log(conditionType);
+  }
+
+  public updateDmn(productName: string, templateName: string, node: NodeModel, conditionType: ConditionTypeEnum) {
+    console.log(node);
+  }
+
+  public deleteDmn(productName: string, templateName: string, node: NodeModel, conditionType: ConditionTypeEnum) {
+    console.log(node);
+  }
+
+  private _fillElementData(node: NodeModel, conditions: ConditionsPropertyModel[]): void {
+    const json = JSON.parse(atob(node.content));
+    const properties: string[] = json['properties'];
+    const conditionsWithOutproperty: ConditionNodes[] = [];
+    properties.forEach((p) => this.elementData.push(new ConditionNodes(p)));
+    conditions.forEach((c) => {
+      const found = this.elementData.find(
+        (e) => e.property.toLowerCase() === c.property.toLowerCase(),
+      );
+      if (typeof found === 'undefined') {
+        const con = new ConditionNodes(c.property + TemplateConditionsComponent.NOT_FOUND);
+        this._fillConditionNodes(con, c);
+        conditionsWithOutproperty.push(con);
+      } else {
+        this._fillConditionNodes(found, c);
+      }
+    });
+    this.elementData = this.elementData.concat(conditionsWithOutproperty);
+    this.table.renderRows();
+  }
+
+  private _fillConditionNodes(
+    conditionNodes: ConditionNodes,
+    conditionsProperty: ConditionsPropertyModel,
+  ): void {
+    conditionNodes.value = conditionsProperty.conditions[ConditionTypeEnum.VALUE];
+    conditionNodes.visible = conditionsProperty.conditions[ConditionTypeEnum.VISIBLE];
+    conditionNodes.disable = conditionsProperty.conditions[ConditionTypeEnum.DISABLE];
+    conditionNodes.alert = conditionsProperty.conditions[ConditionTypeEnum.ALERT];
+    conditionNodes.validate = conditionsProperty.conditions[ConditionTypeEnum.VALIDATE];
+  }
 }
 
 export class ConditionNodes {
-  public value: NodeModel | null = null;
-  public visible: NodeModel | null = null;
-  public disable: NodeModel | null = null;
-  public alert: NodeModel | null = null;
-  public validate: NodeModel | null = new NodeModel();
+  public value: NodeModel | undefined;
+  public visible: NodeModel | undefined;
+  public disable: NodeModel | undefined;
+  public alert: NodeModel | undefined;
+  public validate: NodeModel | undefined;
 
   constructor(public property: string) {}
 }
