892c7a10f08bc8c3c8895db932c51a7f395c7951
Hernael <hperer@movealong.tech>
2024-04-26 01:08:41 -0400

    fix selectcomponent

--- src/app/saman-core/common/lib/configurable-data-entity/custom/components/saman-select/saman-select.form.ts
+++ src/app/saman-core/common/lib/configurable-data-entity/custom/components/saman-select/saman-select.form.ts
@@ -5,7 +5,7 @@ import EditDisplay from './editForm/select.edit.display';
 import EditValidation from './editForm/select.edit.validation';
 import EditLayout from './editForm/select.edit.layout';
 
-const componentForm = (Formio as any).Components.components.select.editForm;
+const componentForm = (Formio as any).Components.components.list.editForm;
 
 export default function(...extend) {
   return componentForm([
