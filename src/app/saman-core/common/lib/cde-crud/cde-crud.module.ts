34f841eb50303a8354b1e7825b491ccf6efc082b
Hernael <hperer@movealong.tech>
2024-06-12 01:14:40 -0400

    add button and dialog module dependencies

--- src/app/saman-core/common/lib/cde-crud/cde-crud.module.ts
+++ src/app/saman-core/common/lib/cde-crud/cde-crud.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 import { ConfigurableDataEntityModule, CdeSearchModule } from '@saman-core/common';
 import { CdeModule } from '@saman-core/data';
 import { CdeCrudComponent } from './cde-crud/cde-crud.component';
@@ -8,24 +15,19 @@ import { CdeCreateComponent } from './cde-create/cde-create.component';
 import { CdeViewComponent } from './cde-view/cde-view.component';
 
 @NgModule({
-  declarations: [
-    CdeCreateComponent,
-    CdeCrudComponent,
-    CdeEditComponent,
-    CdeViewComponent,
-  ],
+  declarations: [CdeCreateComponent, CdeCrudComponent, CdeEditComponent, CdeViewComponent],
   imports: [
     CommonModule,
     ConfigurableDataEntityModule,
     CdeSearchModule,
     CdeModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatButtonModule,
   ],
   providers: [],
-  exports: [
-    CdeCreateComponent,
-    CdeCrudComponent,
-    CdeEditComponent,
-    CdeViewComponent,
-  ],
+  exports: [CdeCreateComponent, CdeCrudComponent, CdeEditComponent, CdeViewComponent],
 })
 export class CdeCrudModule {}
