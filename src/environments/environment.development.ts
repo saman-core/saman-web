fb9a923042fb7822fd507c698b9513459e5f9a89
Hernael <hperer@movealong.tech>
2024-08-21 12:03:46 -0400

    create environment aws

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,39 +1,15 @@
 export const environment = {
   datasourcesFormat: {
     format1: 'http://localhost:{PORT}/{RESOURCE}',
-    format2: 'https://{SERVER}.sige3.link/{RESOURCE}',
-    cde1: 'https://{SERVER}.sige3.link',
-    resources1: 'https://{RESOURCE}.sige3.link/{RESOURCE}',
+    format2: 'https://{SERVER}.nacxio.com:30443/{RESOURCE}',
+    cde1: 'https://{SERVER}.nacxio.com:30443',
+    resources1: 'https://{RESOURCE}.nacxio.com:30443/{RESOURCE}',
   },
   authConfig: {
-    // Url of the Identity Provider
-    issuer: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_k2IuT3Myb',
-    logoutUrl:
-      'https://saman-core.auth.us-east-1.amazoncognito.com/logout?client_id=5arnotdu906unc42a61q5gh0nu&response_type=code&scope=openid+profile+email&redirect_uri=' +
-      window.location.origin,
-
-    // URL of the SPA to redirect the user to after login
-    redirectUri: window.location.origin,
-
-    // The SPA's id. The SPA is registerd with this id at the auth-server
-    // clientId: 'server.code',
-    clientId: '5arnotdu906unc42a61q5gh0nu',
-
-    // Just needed if your auth server demands a secret. In general, this
-    // is a sign that the auth server is not configured with SPAs in mind
-    // and it might not enforce further best practices vital for security
-    // such applications.
-    // dummyClientSecret: 'secret',
-
-    responseType: 'code',
-
-    // set the scope for the permissions the client should request
-    // The first four are defined by OIDC.
-    // Important: Request offline_access to get a refresh token
-    // The api scope is a usecase specific one
+    issuer: 'https://identity.nacxio.com:31443/realms/saman',
+    clientId: 'saman-web',
     scope: 'openid profile email',
-    strictDiscoveryDocumentValidation: false,
-
-    showDebugInformation: true,
+    redirectUri: window.location.origin,
+    responseType: 'code',
   },
 };
