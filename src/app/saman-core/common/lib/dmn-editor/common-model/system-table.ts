5206f56a766c404dd5918e6e4a382308a0d67949
Hernael <hperer@movealong.tech>
2024-09-23 18:25:44 -0400

    change system-table

--- src/app/saman-core/common/lib/dmn-editor/common-model/system-table.ts
+++ src/app/saman-core/common/lib/dmn-editor/common-model/system-table.ts
@@ -1,104 +1,104 @@
 export const SYSTEM_TABLE = `<?xml version="1.0" encoding="UTF-8"?>
-<dmn:definitions xmlns:dmn="http://www.omg.org/spec/DMN/20180521/MODEL/" xmlns="saman-core/property/generatesMovements_value" xmlns:feel="http://www.omg.org/spec/DMN/20180521/FEEL/" xmlns:kie="http://www.drools.org/kie/dmn/1.2" xmlns:dmndi="http://www.omg.org/spec/DMN/20180521/DMNDI/" xmlns:di="http://www.omg.org/spec/DMN/20180521/DI/" xmlns:dc="http://www.omg.org/spec/DMN/20180521/DC/" id="_187F4C76-FFEE-438A-BA01-9D8F86961B86" name="generatesMovements_value" typeLanguage="http://www.omg.org/spec/DMN/20180521/FEEL/" namespace="saman-core/property/generatesMovements_value">
+<dmn:definitions xmlns:dmn="http://www.omg.org/spec/DMN/20180521/MODEL/" xmlns="saman-core/system-table" xmlns:feel="http://www.omg.org/spec/DMN/20180521/FEEL/" xmlns:kie="http://www.drools.org/kie/dmn/1.2" xmlns:dmndi="http://www.omg.org/spec/DMN/20180521/DMNDI/" xmlns:di="http://www.omg.org/spec/DMN/20180521/DI/" xmlns:dc="http://www.omg.org/spec/DMN/20180521/DC/" id="_5AE59027-5D65-46BD-A77B-FDCC08BB3CC1" name="system-table" typeLanguage="http://www.omg.org/spec/DMN/20180521/FEEL/" namespace="saman-core/system-table">
   <dmn:extensionElements/>
-  <dmn:businessKnowledgeModel id="_8D62B497-C2C8-4C60-BE16-2619138AAC16" name="systemTableNumber">
+  <dmn:businessKnowledgeModel id="_41C669E2-E8EB-46EC-8E10-0E4A6BB92BA0" name="systemTableString">
     <dmn:extensionElements/>
-    <dmn:variable id="_D5774FC8-DB13-4AAD-A57C-4ECE566FB6C9" name="systemTableNumber" typeRef="number"/>
-    <dmn:encapsulatedLogic id="_5F7DDF61-E0F2-4C6F-B4B4-8DE7E585F45C" kind="Java">
-      <dmn:formalParameter id="_5A1ED60F-9727-4895-B4D7-DBA515865484" name="systemTableName" typeRef="string"/>
-      <dmn:formalParameter id="_8C2CE7A1-64C4-4B05-9C87-34A6688257EE" name="systemTableProperty" typeRef="string"/>
-      <dmn:formalParameter id="_1CD038B5-95A7-4ED3-9835-D2425F0EDEEF" name="conditions" typeRef="context"/>
-      <dmn:context id="_43D81CAC-AE81-4306-BD97-AC1055FF9BEA">
+    <dmn:variable id="_24186599-5E17-419A-802C-E84B5553BE4F" name="systemTableString" typeRef="string"/>
+    <dmn:encapsulatedLogic id="_52C04384-41C5-45B7-B4A3-5CEC7416EB57" kind="Java">
+      <dmn:formalParameter id="_98B17DAA-29AA-4D70-B8AE-526E9DB4F069" name="name" typeRef="string"/>
+      <dmn:formalParameter id="_7D98F932-4648-4B00-AA76-2EF40A71BF1E" name="property" typeRef="string"/>
+      <dmn:formalParameter id="_B706F17D-9E87-48AC-876D-70E5B38A0E4A" name="filters" typeRef="context"/>
+      <dmn:context id="_2EF4BDDC-999B-4C53-A0D1-71EFE7DCD88B">
         <dmn:contextEntry>
-          <dmn:variable id="_A159E7A4-2BA7-4E84-B046-8358293BF2B0" name="class" typeRef="string"/>
-          <dmn:literalExpression id="_8FD1E332-60C3-4C16-BA9B-A5FB2C72F3A7">
+          <dmn:variable id="_0C823D85-DEDF-4320-9DF2-1097B5A5E732" name="class" typeRef="string"/>
+          <dmn:literalExpression id="_2541218C-7FE6-437F-8DB5-7D4D3138C18F">
             <dmn:text>"io.samancore.condition_template.client.SystemTableWrapperClient"</dmn:text>
           </dmn:literalExpression>
         </dmn:contextEntry>
         <dmn:contextEntry>
-          <dmn:variable id="_925CC0E7-783A-4F7C-886B-8C322A5E4CF9" name="method signature" typeRef="string"/>
-          <dmn:literalExpression id="_C4A25513-AE9C-4FEA-A455-D71BCC0E1AE5">
-            <dmn:text>"fetchNumber(java.lang.String, java.lang.String, java.util.Map)"</dmn:text>
-          </dmn:literalExpression>
-        </dmn:contextEntry>
-      </dmn:context>
-    </dmn:encapsulatedLogic>
-  </dmn:businessKnowledgeModel>
-  <dmn:businessKnowledgeModel id="_55B272DE-2012-4765-83A0-1AD1E58BB4BB" name="systemTableString">
-    <dmn:extensionElements/>
-    <dmn:variable id="_FBAA97A8-698D-4252-BE3A-3F83A374AE0C" name="systemTableString" typeRef="string"/>
-    <dmn:encapsulatedLogic id="_C7C0AB22-F879-4078-905E-2EAA8BEB499E" kind="Java">
-      <dmn:formalParameter id="_CE9B8BE3-1185-4168-9143-EA5FDEC381E7" name="systemTableName" typeRef="string"/>
-      <dmn:formalParameter id="_5055B91B-F28E-4918-9F14-5F9890AF9BCB" name="systemTableProperty" typeRef="string"/>
-      <dmn:formalParameter id="_43252BC0-CF47-4AD3-9292-120EB55FD713" name="conditions" typeRef="context"/>
-      <dmn:context id="_42C51228-E67F-4537-8A7D-F116299A937A">
-        <dmn:contextEntry>
-          <dmn:variable id="_6FF6BB16-FC58-4476-96D4-B490B16835C2" name="class" typeRef="string"/>
-          <dmn:literalExpression id="_BD68B3B3-B711-4DEB-8129-7934285BDBCD">
-            <dmn:text>"io.samancore.condition_template.client.SystemTableWrapperClient"</dmn:text>
-          </dmn:literalExpression>
-        </dmn:contextEntry>
-        <dmn:contextEntry>
-          <dmn:variable id="_301AD3DB-1AAC-497F-9535-6C28508D19BD" name="method signature" typeRef="string"/>
-          <dmn:literalExpression id="_A51418AF-D532-4253-B3FA-F5449C80089B">
+          <dmn:variable id="_7F7FA4EE-4C3A-42B5-B512-894B24E3C692" name="method signature" typeRef="string"/>
+          <dmn:literalExpression id="_ABCB7689-B035-4D0F-B6F2-B5B8B24F1032">
             <dmn:text>"fetchString(java.lang.String, java.lang.String, java.util.Map)"</dmn:text>
           </dmn:literalExpression>
         </dmn:contextEntry>
       </dmn:context>
     </dmn:encapsulatedLogic>
   </dmn:businessKnowledgeModel>
+  <dmn:businessKnowledgeModel id="_B4D076DA-9572-421C-BFAE-7660FF68A9C4" name="systemTableNumber">
+    <dmn:extensionElements/>
+    <dmn:variable id="_13D1D2CB-BFF7-4186-8291-E980AB682355" name="systemTableNumber" typeRef="number"/>
+    <dmn:encapsulatedLogic id="_F8135280-243F-4450-B417-B2208CF95BD1" kind="Java">
+      <dmn:formalParameter id="_345F9D67-0F7F-4903-BD7F-E09701D02692" name="name" typeRef="string"/>
+      <dmn:formalParameter id="_9BE0C068-6327-4EEB-B255-EEC0FAFF34A4" name="property" typeRef="string"/>
+      <dmn:formalParameter id="_F6C9C21D-1B3E-4253-88C2-16B263685570" name="filters" typeRef="context"/>
+      <dmn:context id="_82537596-7D8F-48FF-A12C-7237E63C3D78">
+        <dmn:contextEntry>
+          <dmn:variable id="_574D0349-095F-4C69-9D98-08A9227B0F67" name="class" typeRef="string"/>
+          <dmn:literalExpression id="_76004643-496F-4735-A595-DEBB72AC3C04">
+            <dmn:text>"io.samancore.condition_template.client.SystemTableWrapperClient"</dmn:text>
+          </dmn:literalExpression>
+        </dmn:contextEntry>
+        <dmn:contextEntry>
+          <dmn:variable id="_259D0284-0EBC-4548-AE8E-D1042562B9E3" name="method signature" typeRef="string"/>
+          <dmn:literalExpression id="_FEA67E1A-D40D-4E6C-9A91-F48AEFE49334">
+            <dmn:text>"fetchNumber(java.lang.String, java.lang.String, java.util.Map)"</dmn:text>
+          </dmn:literalExpression>
+        </dmn:contextEntry>
+      </dmn:context>
+    </dmn:encapsulatedLogic>
+  </dmn:businessKnowledgeModel>
   <dmndi:DMNDI>
-    <dmndi:DMNDiagram id="_64110A90-BCCA-462F-983B-2FBD2CD39E92" name="DRG">
+    <dmndi:DMNDiagram id="_68F295DD-E8FD-422F-A1D2-427B9C20CD7A" name="DRG">
       <di:extension>
         <kie:ComponentsWidthsExtension>
-          <kie:ComponentWidths dmnElementRef="_43D81CAC-AE81-4306-BD97-AC1055FF9BEA">
+          <kie:ComponentWidths dmnElementRef="_2EF4BDDC-999B-4C53-A0D1-71EFE7DCD88B">
             <kie:width>50</kie:width>
             <kie:width>100</kie:width>
-            <kie:width>446</kie:width>
+            <kie:width>470</kie:width>
           </kie:ComponentWidths>
-          <kie:ComponentWidths dmnElementRef="_8FD1E332-60C3-4C16-BA9B-A5FB2C72F3A7">
-            <kie:width>446</kie:width>
+          <kie:ComponentWidths dmnElementRef="_2541218C-7FE6-437F-8DB5-7D4D3138C18F">
+            <kie:width>470</kie:width>
           </kie:ComponentWidths>
-          <kie:ComponentWidths dmnElementRef="_C4A25513-AE9C-4FEA-A455-D71BCC0E1AE5">
-            <kie:width>446</kie:width>
+          <kie:ComponentWidths dmnElementRef="_ABCB7689-B035-4D0F-B6F2-B5B8B24F1032">
+            <kie:width>470</kie:width>
           </kie:ComponentWidths>
-          <kie:ComponentWidths dmnElementRef="_5F7DDF61-E0F2-4C6F-B4B4-8DE7E585F45C">
+          <kie:ComponentWidths dmnElementRef="_52C04384-41C5-45B7-B4A3-5CEC7416EB57">
             <kie:width>50</kie:width>
-            <kie:width>616</kie:width>
+            <kie:width>640</kie:width>
           </kie:ComponentWidths>
-          <kie:ComponentWidths dmnElementRef="_42C51228-E67F-4537-8A7D-F116299A937A">
+          <kie:ComponentWidths dmnElementRef="_82537596-7D8F-48FF-A12C-7237E63C3D78">
             <kie:width>50</kie:width>
             <kie:width>100</kie:width>
-            <kie:width>389</kie:width>
+            <kie:width>379</kie:width>
           </kie:ComponentWidths>
-          <kie:ComponentWidths dmnElementRef="_BD68B3B3-B711-4DEB-8129-7934285BDBCD">
-            <kie:width>389</kie:width>
+          <kie:ComponentWidths dmnElementRef="_76004643-496F-4735-A595-DEBB72AC3C04">
+            <kie:width>379</kie:width>
           </kie:ComponentWidths>
-          <kie:ComponentWidths dmnElementRef="_A51418AF-D532-4253-B3FA-F5449C80089B">
-            <kie:width>389</kie:width>
+          <kie:ComponentWidths dmnElementRef="_FEA67E1A-D40D-4E6C-9A91-F48AEFE49334">
+            <kie:width>379</kie:width>
           </kie:ComponentWidths>
-          <kie:ComponentWidths dmnElementRef="_C7C0AB22-F879-4078-905E-2EAA8BEB499E">
+          <kie:ComponentWidths dmnElementRef="_F8135280-243F-4450-B417-B2208CF95BD1">
             <kie:width>50</kie:width>
-            <kie:width>559</kie:width>
+            <kie:width>549</kie:width>
           </kie:ComponentWidths>
         </kie:ComponentsWidthsExtension>
       </di:extension>
-      <dmndi:DMNShape id="dmnshape-drg-_8D62B497-C2C8-4C60-BE16-2619138AAC16" dmnElementRef="_8D62B497-C2C8-4C60-BE16-2619138AAC16" isCollapsed="false">
+      <dmndi:DMNShape xmlns:p0="https://kiegroup.org/dmn/_0F165F0B-0342-4EEC-8D01-38D8C71DDA8C" id="dmnshape-drg-_41C669E2-E8EB-46EC-8E10-0E4A6BB92BA0" dmnElementRef="p0:_41C669E2-E8EB-46EC-8E10-0E4A6BB92BA0" isCollapsed="false">
         <dmndi:DMNStyle>
           <dmndi:FillColor red="255" green="255" blue="255"/>
           <dmndi:StrokeColor red="0" green="0" blue="0"/>
           <dmndi:FontColor red="0" green="0" blue="0"/>
         </dmndi:DMNStyle>
-        <dc:Bounds x="119" y="70" width="100" height="50"/>
+        <dc:Bounds x="283" y="101" width="144" height="50"/>
         <dmndi:DMNLabel/>
       </dmndi:DMNShape>
-      <dmndi:DMNShape id="dmnshape-drg-_55B272DE-2012-4765-83A0-1AD1E58BB4BB" dmnElementRef="_55B272DE-2012-4765-83A0-1AD1E58BB4BB" isCollapsed="false">
+      <dmndi:DMNShape xmlns:p1="https://kiegroup.org/dmn/_0F165F0B-0342-4EEC-8D01-38D8C71DDA8C" id="dmnshape-drg-_B4D076DA-9572-421C-BFAE-7660FF68A9C4" dmnElementRef="p1:_B4D076DA-9572-421C-BFAE-7660FF68A9C4" isCollapsed="false">
         <dmndi:DMNStyle>
           <dmndi:FillColor red="255" green="255" blue="255"/>
           <dmndi:StrokeColor red="0" green="0" blue="0"/>
           <dmndi:FontColor red="0" green="0" blue="0"/>
         </dmndi:DMNStyle>
-        <dc:Bounds x="119" y="157" width="100" height="50"/>
+        <dc:Bounds x="283" y="162" width="146" height="51"/>
         <dmndi:DMNLabel/>
       </dmndi:DMNShape>
     </dmndi:DMNDiagram>
