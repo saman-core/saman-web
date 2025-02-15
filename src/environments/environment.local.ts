0dbbd9af50ffd26fc9b0c8eba50c8f6862f920a4
Hernael <hperer@movealong.tech>
2025-02-15 16:50:03 -0400

    chane domain

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,12 +1,12 @@
 export const environment = {
   datasourcesFormat: {
     format1: 'http://localhost:{PORT}/{RESOURCE}',
-    format2: 'https://{SERVER}.nacxio.com:30443/{RESOURCE}',
-    cde1: 'https://{SERVER}.nacxio.com:30443',
-    resources1: 'https://{RESOURCE}.nacxio.com:30443/{RESOURCE}',
+    format2: 'https://{SERVER}.saman-tech.com:35443/{RESOURCE}',
+    cde1: 'https://{SERVER}.saman-tech.com:35443',
+    resources1: 'https://{RESOURCE}.saman-tech.com:35443/{RESOURCE}',
   },
   authConfig: {
-    issuer: 'https://identity.nacxio.com:31443/realms/saman',
+    issuer: 'https://identity.saman-tech.com:37443/realms/saman',
     clientId: 'saman-web',
     scope: 'openid profile email',
     redirectUri: window.location.origin,
